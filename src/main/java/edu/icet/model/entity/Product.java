8f63898675cc17ce3f5f051e009bfbad30c6ca65
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-04-30 06:41:15 +0530

    create all packages and classes

---
+++ src/main/java/edu/icet/model/entity/Product.java
@@ -0,0 +1,17 @@
+package edu.icet.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private String category;
+}
